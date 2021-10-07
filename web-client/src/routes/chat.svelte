64d68dea2f8762d4e8b5ac5e1d61da2b47637270
said <kordiseps@gmail.com>
2021-10-08 00:50:08 +0300

    Style chatbox simply

--- web-client/src/routes/chat.svelte
+++ web-client/src/routes/chat.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { fade, fly,blur } from "svelte/transition";
+    import { fade, fly, blur } from "svelte/transition";
     import {
         apiGetLastMessageId,
         apiGetOlderMessagesFrom,
@@ -8,36 +8,38 @@
     let username = "";
     let shown = false;
     export function startFetching(un) {
-        console.log("fetching data user :", un);
+        //console.log("fetching data user :", un);
         username = un;
         shown = true;
-        messagesPromise = initial();
+        initial();
     }
+
+    //
 
     async function initial() {
         const latestMessage = await apiGetLastMessageId($authStore, username);
         //console.log("latest ok");
         const moreMessages = await more(latestMessage.messageId);
         //console.log("more ok", moreMessages);
-        const messages = Promise.all(
-            moreMessages.messageIdList.map(async (x) => {
-                const msg = await apiGetSingleMessage($authStore, x);
-                console.log(msg);
-                return msg;
-            })
+        const messages = await Promise.all(
+            moreMessages.messageIdList
+                .map(async (x) => {
+                    const msg = await apiGetSingleMessage($authStore, x);
+                    //console.log(msg);
+                    return msg;
+                })
+                .reverse()
         );
-        //console.log("single ok", messages);
-        return messages;
+
+        $messageList = [...messages];
     }
 
     function more(messageId) {
         return apiGetOlderMessagesFrom($authStore, messageId);
     }
 
-    let messagesPromise = Promise.resolve();
-
     import { createEventDispatcher, onMount } from "svelte";
-    import { authStore } from "../store";
+    import { authStore, messageList } from "../store";
 
     const dispatch = createEventDispatcher();
 
@@ -45,26 +47,114 @@
         dispatch("goList");
         shown = false;
     }
+
+    let newmessage;
+
+    function sendMessage(event) {
+        $messageList = [
+            ...$messageList,
+            {
+                receiverUser: "user1",
+                senderUser: "said",
+                message: newmessage,
+            },
+        ];
+        newmessage = "";
+    }
+
+    //initial
+    //scroll up
+    //send
+    //ilerde socket ile
 </script>
 
 {#if shown}
-    <div class="chat-box" transition:fly>
-        <button on:click={goList}>back to list</button>
-        <p>selam {username}</p>
-        {#await messagesPromise}
-            <p>loading</p>
-        {:then data}
-            <p>{JSON.stringify(data, null, 2)}</p>
-        {/await}
+    <div class="chat-box-wrapper" transition:fly>
+        <div class="chat-header">
+            <div class="back" on:click={goList}>&lt;back</div>
+            <div class="opponent">{username}</div>
+        </div>
+
+        <div class="chat-messages">
+            <ul>
+                {#each $messageList as msg}
+                    <li>
+                        <div class="msg-item">
+                            {#if msg.senderUser == username}
+                                <div class="received">
+                                    {msg.senderUser} : {msg.message}
+                                </div>
+                            {:else}
+                                <div class="sended">
+                                    {msg.message}
+                                </div>
+                            {/if}
+                        </div>
+                    </li>
+                {/each}
+            </ul>
+        </div>
+        <div class="chat-send-message">
+            <div class="input">
+                <input type="text" bind:value={newmessage} />
+            </div>
+            <div class="submit">
+                <button on:click={sendMessage}>send</button>
+            </div>
+        </div>
     </div>
 {/if}
 
 <style>
-    .chat-box {
+    .chat-box-wrapper {
         background-color: rgb(0 0 0 / 10%);
         text-align: center;
-        padding: 1em;
+        
         max-width: 240px;
         margin: 0 auto;
+        height: 100vh;
+    }
+    .chat-messages{
+        overflow-y:auto; 
+        height: 70vh;
+        padding: 1em;
+    }
+    .chat-header {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        height: 5vh;
+        padding: 1em;
+
+    }
+    .chat-send-message {
+        height: 5vh;
+        display: flex;
+        padding: 1em;
+
+    }
+    input{
+        width: 100%;
+    }
+    .sended {
+        text-align: right;
+    }
+    .received {
+        text-align: left;
+    }
+    .back {
+        cursor: pointer;
+        color: white;
+    }
+    .opponent {
+        color: white;
+    }
+    li + li {
+        margin-top: 0.5em;
+    }
+    ul {
+        list-style: none;
+        padding-left: 0;
+        bottom: 0;
     }
 </style>
